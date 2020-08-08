2c21d82202a7bff798dada0f54f542a1eac7e858
Hưng Chế Quang <paulpassior@gmail.com>
2020-08-08 23:52:13 +0700

    added ssl

--- src/models/index.js
+++ src/models/index.js
@@ -3,23 +3,30 @@ const { createProductModel } = require('./product')
 const { createUserModel } = require('./user')
 const { createFruitModel } = require('./fruit')
 const { createInstitutionModel } = require('./institution')
-const { createAddressModel } = require('./address') 
-const { createAuctionBidModel } = require('./auction_bid') 
-const { createAuctionModel } = require('./auction') 
-const { createWalletModel } = require('./wallet') 
-const { createProductMediaModel } = require('./product_media') 
-const { createProductUpdateModel } = require('./product_update') 
-const { createProductUpdateRequestModel } = require('./product_update_request') 
-const { createIdentityModel } = require('./identity') 
+const { createAddressModel } = require('./address')
+const { createAuctionBidModel } = require('./auction_bid')
+const { createAuctionModel } = require('./auction')
+const { createWalletModel } = require('./wallet')
+const { createProductMediaModel } = require('./product_media')
+const { createProductUpdateModel } = require('./product_update')
+const { createProductUpdateRequestModel } = require('./product_update_request')
+const { createIdentityModel } = require('./identity')
 const identity = require('./identity')
 
 
 // like entities
-const createModels = async ({ dbName, dbUser, dbPass, dbHost }) => {
+const createModels = async ({ dbName, dbUser, dbPass, dbHost, dbSslCa, dbSslCert, dbSslKey }) => {
     // establish a connection
     const sequelize = new Sequelize(dbName, dbUser, dbPass, {
         host: dbHost,
-        dialect: 'mysql'
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: {
+                ca: dbSslCa,
+                cert: dbSslCert,
+                key: dbSslKey
+            }
+        }
     })
 
     // wait for authentication
@@ -30,14 +37,14 @@ const createModels = async ({ dbName, dbUser, dbPass, dbHost }) => {
         User: createUserModel(sequelize),
         Fruit: createFruitModel(sequelize),
         Institution: createInstitutionModel(sequelize),
-        Address : createAddressModel (sequelize),
-        AuctionBid : createAuctionBidModel (sequelize),
-        Auction : createAuctionModel (sequelize),
-        Wallet : createWalletModel (sequelize),
-        ProductMedia : createProductMediaModel (sequelize),
-        ProductUpdate : createProductUpdateModel(sequelize),
-        ProductUpdateRequest : createProductUpdateRequestModel(sequelize),
-        Identity:createIdentityModel(sequelize),
+        Address: createAddressModel(sequelize),
+        AuctionBid: createAuctionBidModel(sequelize),
+        Auction: createAuctionModel(sequelize),
+        Wallet: createWalletModel(sequelize),
+        ProductMedia: createProductMediaModel(sequelize),
+        ProductUpdate: createProductUpdateModel(sequelize),
+        ProductUpdateRequest: createProductUpdateRequestModel(sequelize),
+        Identity: createIdentityModel(sequelize),
     }
 }
 
